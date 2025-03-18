3335aa6001bcb8f08df92e01a79af216fea32448
Ruslana <tretiakovaruslana7@gmail.com>
2025-03-18 11:19:04 +0100

    feat: add initial Header and Footer components

---
+++ src/widgets/header/ui/header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <>
+          <div>
+            <h2 className="header__title">Potteroloves</h2>
+          </div>
+        </>
+      </header>
+    </>
+  );
+};
